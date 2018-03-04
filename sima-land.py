2924b1820691cd214d07f90d87a9d534ce7df1f5
moonz <mihail.moonz@gmail.com>
2018-03-04 18:31:40 +0300

    beta

--- sima-land.py
+++ sima-land.py
@@ -1,4 +1,4 @@
-import requests
+import requests, csv
 from bs4 import BeautifulSoup
 '''
 План работ :
@@ -20,6 +20,42 @@ def get_total_page(html):
 
     return int(total_pages)
 
+def write_csv(date):
+    with open('spisok.csv', 'a') as f:
+        writer = csv.writer(f)
+
+        writer.writerow( (date['title'],
+                          date['url'],
+                          date['price']) )
+
+
+
+
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    ads = soup.find('div', class_='catalog__list').find_all('div', class_='catalog__item-main')
+
+    for ad in ads:
+        try:
+            title = ad.find('div', class_='catalog__item-details delivery-details').find(class_='link').text.strip()
+        except:
+            title = ''
+        try:
+            url = 'https://sima-land.ru' + ad.find('div', class_='catalog__item-details delivery-details').find('a').get('href')
+        except:
+            url = ''
+
+        try:
+            price = ad.find('div', class_='catalog__item-price ').find(class_='price__val').text
+        except:
+            price = ''
+
+        data = {'title':title,
+                'url':url,
+                'price':price}
+
+        write_csv(data)
+
 
 
 
@@ -28,9 +64,11 @@ def main():
     base_url = 'https://www.sima-land.ru/igrushki/razvivayuschie-i-obuchayuschie-igrushki/'
     page_part = 'p'
     total_pages = get_total_page(get_html(url))
-    for i in range(1, 90):
+    for i in range(1, 3):
         url_gen = base_url + page_part + str(i)
-        print(url_gen)
+        html = get_html(url_gen)
+
+
 
 
 
