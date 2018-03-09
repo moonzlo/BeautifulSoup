069eed4cfaa827d06dcb012d58cb41051d88edfb
moonz <mihail.moonz@gmail.com>
2018-03-09 11:51:45 +0300

    Сбор url юсеров в deviantart

--- DevianUsers.py
+++ DevianUsers.py
@@ -1,26 +1,38 @@
-import requests
+import requests, time
 from bs4 import BeautifulSoup
 
-url = 'https://www.deviantart.com/newest/'
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'lxml')
 
-test = soup.find(id='browse-results-page-1').find_all(class_='artist')
-b = str()
-c = open('links.txt', 'r')
-file = str(c.readlines())
-c.close()
-for sup in test:
-    user = sup.find('a').get('href')
-    if user in file:
-        continue
-    else:
-        a = str(user) + '\n'
-        b += a
+def parsing():
+    set = int(0)
+    # Собираем из 4х страниц Url'ы всех пользователей (около 22 с каждой)
+    links = ['https://www.deviantart.com/digitalart/newest/',
+             'https://www.deviantart.com/traditional/newest/',
+             'https://www.deviantart.com/photography/newest/',
+             'https://www.deviantart.com/manga/newest/']
+    while set != int(4):
+        r = requests.get(links[set])
+        soup = BeautifulSoup(r.text, 'lxml')
 
-c = open('links.txt', 'a')
-c.writelines(b)
-c.close()
+        test = soup.find(id='browse-results-page-1').find_all(class_='artist')
+        b = str()
+        c = open('links.txt', 'r')
+        file = str(c.readlines())
+        c.close()
+        for sup in test:  # Реализуем проверку каждой ссылки на наличие её в Уже сформированном листе.
+            user = sup.find('a').get('href')
+            if user in file:  # Если ссылка есть в списке, она игнорируется.
+                continue
+            else:
+                a = str(user) + '\n'  # Данная кострукция реализована с целью последовательной записи в новую строку.
+                b += a  # К данной кострукции пришлось прибегнуть по причине неверной записи ссылок.
+
+        c = open('links.txt', 'a')  # Осуществляем запись в файл (он должен быть создан).
+        c.writelines(b)  # Посторочно записываем содержимое переменной с сылками.
+        c.close()
+        set += int(1)  # Конструкция для перебора донорских ссылок.
 
 
-
+b = range(1, 10)
+for i in b:  # реализация долгосрочного сбора, с интервалом в 10 минут.
+    time.sleep(600)
+    parsing()
